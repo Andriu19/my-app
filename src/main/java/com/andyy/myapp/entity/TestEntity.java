d831ff7fe8cdaf90691e4c80efdf3bf8184cc2c7
andriu19 <andrea.cordova.mejia@gmail.com>
2024-01-23 14:02:04 -0600

    Nombre del proyecto: my-app, Descripcion: Version final del proyectp my-app

---
+++ src/main/java/com/andyy/myapp/entity/TestEntity.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.andyy.myapp.entity;
+
+/**
+ * @author Andyy Cordova Mejia 
+ */
+public class TestEntity {
+	/**
+	 * Propiedad de prueba para la muestra de compilacion del archivo TestEntity
+	 */
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+
+}
